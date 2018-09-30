949b129c47011f8b49823518a3ae8d7b89e89fc8
puer <vova@mytnik.net>
2018-09-30 22:26:41 +0300

    Add JavaDoc comments

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,17 @@
+/**
+*Just Hello Word class
+*
+*@author Vladimir Mytnik(vova@mytnik.net)
+*@since 30.09.2018
+*/
+
 package ru.job4j.calculate;
 
 public class Calculate {
+  /**
+  *Main method. Print string to console.
+  *@param args - args
+  */
   public static void main(String[] args) {
     System.out.println("Hello Word!");
   }
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Vladimir Mytnik (vova@mytnik.net)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;
