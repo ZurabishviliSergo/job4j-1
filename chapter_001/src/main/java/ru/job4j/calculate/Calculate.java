69bdea70da585e7e84cc492804e78832c6f75ee5
puer <vova@mytnik.net>
2018-09-30 22:33:08 +0300

    Add JavaDoc comments

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -2,15 +2,15 @@
 *Just Hello Word class
 *
 *@author Vladimir Mytnik(vova@mytnik.net)
-*@since 30.09.2018
+*@version 0.1
 */
 
 package ru.job4j.calculate;
 
 public class Calculate {
   /**
-  *Main method. Print string to console.
-  *@param args - args
+  *Entry point to the class and app. Print string to console.
+  *@param args - string arguments array
   */
   public static void main(String[] args) {
     System.out.println("Hello Word!");
