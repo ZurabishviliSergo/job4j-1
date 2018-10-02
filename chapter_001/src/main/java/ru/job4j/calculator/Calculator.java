cc60184e0964936cee29a912b3d1344ab1e6e428
mrpuer-mini <vova@mytnik.net>
2018-10-02 13:29:49 +0300

    New Calculator class and tests

---
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -0,0 +1,25 @@
+package ru.job4j.calculator;
+
+public class Calculator {
+    public double result;
+
+    public void add(double first, double second) {
+        this.result = first + second;
+    }
+
+    public void subtract(double first, double second) {
+        this.result = first - second;
+    }
+
+    public void div(double first, double second) {
+        this.result = first / second;
+    }
+
+    public void multiple(double first, double second) {
+        this.result = first * second;
+    }
+
+    public double getResult() {
+        return this.result;
+    }
+}
