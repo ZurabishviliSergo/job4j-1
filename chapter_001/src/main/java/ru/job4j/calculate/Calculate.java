b52425b73e1b3469c0594a6c721f575bf8f269b5
mrpuer-mini <vova@mytnik.net>
2018-10-02 09:15:30 +0300

    formatting

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,26 +1,1 @@
-/**
-*Just Hello Word class
-*
-*@author Vladimir Mytnik(vova@mytnik.net)
-*@version 0.1
-*/
-
-package ru.job4j.calculate;
-
-public class Calculate {
-  /**
-  *Entry point to the class and app. Print string to console.
-  *@param args - string arguments array
-  */
-  public static void main(String[] args) {
-    System.out.println("Hello Word!");
-  }
-  /**
-  * Method echo.
-  * @param name Your name.
-  * @return Echo plus your name.
-  */
-  public String echo(String name) {
-    return "Echo, echo, echo : " + name;
-  }
-}
+/** *Just Hello Word class * *@author Vladimir Mytnik(vova@mytnik.net) *@version 0.1 */package ru.job4j.calculate;public class Calculate {  /**   *Entry point to the class and app. Print string to console.   *@param args - string arguments array   */  public static void main(String[] args) {    System.out.println("Hello Word!");  }  /**   * Method echo.   * @param name Your name.   * @return Echo plus your name.   */  public String echo(String name) {    return "Echo, echo, echo : " + name;  }}
