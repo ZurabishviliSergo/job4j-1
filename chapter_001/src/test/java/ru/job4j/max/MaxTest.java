af4f3f75473211c005b754ac3e27b53be19640ae
mrpuer-mini <vova@mytnik.net>
2018-10-02 16:35:42 +0300

    New DummyBot class and tests

---
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -0,0 +1,38 @@
+package ru.job4j.max;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class MaxTest {
+    @Test
+    public void whenFirstLessSecond() {
+        Max maxim = new Max();
+        int result = maxim.max(1, 2);
+        assertThat(result, is(2));
+    }
+    @Test
+    public void whenSecondLessFirst() {
+        Max maxim = new Max();
+        int result = maxim.max(2, 1);
+        assertThat(result, is(2));
+    }
+    @Test
+    public void whenEquals() {
+        Max maxim = new Max();
+        int result = maxim.max(2, 2);
+        assertThat(result, is(2));
+    }
+    @Test
+    public void whenSecondNegative() {
+        Max maxim = new Max();
+        int result = maxim.max(2, -1);
+        assertThat(result, is(2));
+    }
+    @Test
+    public void whenFirstEqualsNull() {
+        Max maxim = new Max();
+        int result = maxim.max(0, -1);
+        assertThat(result, is(0));
+    }
+}
