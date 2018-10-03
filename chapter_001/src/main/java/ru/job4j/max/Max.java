a846f47109a9cec59aca2a00df8ef80cd355a7ee
mrpuer-mini <vova@mytnik.net>
2018-10-03 08:43:34 +0300

    Add three args method and tests

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -4,4 +4,9 @@ public class Max {
     public int max(int first, int second) {
         return first > second ? first : second;
     }
+    public int max(int first, int second, int third) {
+        int result = this.max(first, second);
+        result = this.max(result, third);
+        return result;
+    }
 }
--- chapter_001/src/test/java/ru/job4j/max/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -35,4 +35,28 @@ public class MaxTest {
         int result = maxim.max(0, -1);
         assertThat(result, is(0));
     }
+    @Test
+    public void whenFirstMoreSecondAndThird() {
+        Max maxim = new Max();
+        int result = maxim.max(3, 2, 1);
+        assertThat(result, is(3));
+    }
+    @Test
+    public void whenSecondMoreFirstAndThird() {
+        Max maxim = new Max();
+        int result = maxim.max(2, 3, 1);
+        assertThat(result, is(3));
+    }
+    @Test
+    public void whenThirdMoreFirstAndSecond() {
+        Max maxim = new Max();
+        int result = maxim.max(1, 2, 3);
+        assertThat(result, is(3));
+    }
+    @Test
+    public void whenThreeArgsEqual() {
+        Max maxim = new Max();
+        int result = maxim.max(2, 2, 2);
+        assertThat(result, is(2));
+    }
 }
