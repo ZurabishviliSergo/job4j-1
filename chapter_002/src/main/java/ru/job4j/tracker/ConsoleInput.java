55530bb90755db580a8a7899c1902a8187ea51d9
mrpuer-mini <vova@mytnik.net>
2018-10-15 10:30:45 +0300

    Add StubInput and StartUITest

--- chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
@@ -3,6 +3,7 @@ package ru.job4j.tracker;
 import java.util.*;
 
 public class ConsoleInput implements Input {
+    @Override
     public String ask(String question) {
         System.out.println(question);
         Scanner scanner = new Scanner(System.in);
