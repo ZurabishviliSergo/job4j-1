f3ce1b6ffc1a80c9b9ad056a1c83a429f6cc0eb5
mrpuer-mini <vova@mytnik.net>
2018-11-06 18:58:28 +0300

    Chapter3/Collections: PriorityQueue

---
+++ chapter_003/src/main/java/ru/job4j/list/ConvertList2Array.java
@@ -0,0 +1,27 @@
+package ru.job4j.list;
+
+import java.util.List;
+
+/**
+ * Class description.
+ *
+ * @author Vladimir Mytnik (vova@mytnik.net).
+ * @version 0.1.
+ * @since 11/6/18
+ */
+public class ConvertList2Array {
+    public int[][] toArray(List<Integer> list, int rows) {
+        int cells = list.size() / rows;
+        if (cells % rows != 0) { cells++; }
+        int position = 0;
+        int[][] array = new int[rows][cells];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cells; j++) {
+                if (position < list.size()) {
+                    array[i][j] = list.get(position++);
+                }
+            }
+        }
+        return array;
+    }
+}
