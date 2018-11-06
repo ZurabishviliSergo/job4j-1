4dc8312c5999502298b4c6069880f3a50830e800
mrpuer-mini <vova@mytnik.net>
2018-11-06 19:15:03 +0300

    Chapter3/Collections: ConvertMatrix2List

---
+++ chapter_003/src/main/java/ru/job4j/list/ConvertMatrix2List.java
@@ -0,0 +1,24 @@
+package ru.job4j.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class description.
+ *
+ * @author Vladimir Mytnik (vova@mytnik.net).
+ * @version 0.1.
+ * @since 11/6/18
+ */
+
+public class ConvertMatrix2List {
+    public List<Integer> toList(int[][] array) {
+        List<Integer> list = new ArrayList<>();
+        for (int[] outerArr : array) {
+            for (int item : outerArr) {
+                list.add(item);
+            }
+        }
+        return list;
+    }
+}
---
+++ chapter_003/src/test/java/ru/job4j/list/ConvertMatrix2ListTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.list;
+
+import org.junit.Test;
+import java.util.Arrays;
+import java.util.List;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Class description.
+ *
+ * @author Vladimir Mytnik (vova@mytnik.net).
+ * @version 0.1.
+ * @since 11/6/18
+ */
+
+public class ConvertMatrix2ListTest {
+    @Test
+    public void when2on2ArrayThenList4() {
+        ConvertMatrix2List list = new ConvertMatrix2List();
+        int[][] input = {
+                {1, 2},
+                {3, 4}
+        };
+        List<Integer> expect = Arrays.asList(
+                1, 2, 3, 4
+        );
+        List<Integer> result = list.toList(input);
+        assertThat(result, is(expect));
+    }
+}
