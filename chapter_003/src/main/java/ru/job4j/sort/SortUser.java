3d6b475ad3e4ec2f57327795af5d3ea742d482d7
mrpuer-mini <vova@mytnik.net>
2018-11-09 17:00:23 +0300

    Chapter3/Collections: SortUser2 refactor

--- chapter_003/src/main/java/ru/job4j/sort/SortUser.java
+++ chapter_003/src/main/java/ru/job4j/sort/SortUser.java
@@ -14,31 +14,11 @@ public class SortUser {
         return new TreeSet<>(users);
     }
     public List<User> sortNameLength (List<User> users) {
-        class UserCompareByNameLength implements Comparator<User> {
-            public int compare(User lhs, User rhs) {
-                int result = 0;
-                if (lhs.getName().length() != rhs.getName().length()) {
-                    result = lhs.getName().length() > rhs.getName().length() ? 1 : -1;
-                }
-                return result;
-            }
-        }
-        UserCompareByNameLength userCompareByNameLength = new UserCompareByNameLength();
-        Collections.sort(users, userCompareByNameLength);
+        Collections.sort(users, Comparator.comparingInt(o -> o.getName().length()));
         return users;
     }
     public List<User> sortByAllFields (List<User> users) {
-        class UserCompareByAllFields implements Comparator<User> {
-            public int compare(User lhs, User rhs) {
-                int result = lhs.getName().compareTo(rhs.getName());
-                if (result == 0) {
-                    result = lhs.getAge() > rhs.getAge() ? 1 : -1;
-                }
-                return result;
-            }
-        }
-        UserCompareByAllFields userCompareByAllFields = new UserCompareByAllFields();
-        Collections.sort(users, userCompareByAllFields);
+        Collections.sort(users, Comparator.comparing(User::getName).thenComparingInt(User::getAge));
         return users;
     }
 }
