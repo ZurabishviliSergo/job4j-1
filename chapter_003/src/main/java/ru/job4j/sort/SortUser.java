f7150b57b5f07ecf5e45ac4c7afe1cafc45509e2
mrpuer-mini <vova@mytnik.net>
2018-11-09 15:47:27 +0300

    Chapter3/Collections: SortUser2

--- chapter_003/src/main/java/ru/job4j/sort/SortUser.java
+++ chapter_003/src/main/java/ru/job4j/sort/SortUser.java
@@ -13,4 +13,32 @@ public class SortUser {
     public Set<User> sort (List<User> users) {
         return new TreeSet<>(users);
     }
+    public List<User> sortNameLength (List<User> users) {
+        class UserCompareByNameLength implements Comparator<User> {
+            public int compare(User lhs, User rhs) {
+                int result = 0;
+                if (lhs.getName().length() != rhs.getName().length()) {
+                    result = lhs.getName().length() > rhs.getName().length() ? 1 : -1;
+                }
+                return result;
+            }
+        }
+        UserCompareByNameLength userCompareByNameLength = new UserCompareByNameLength();
+        Collections.sort(users, userCompareByNameLength);
+        return users;
+    }
+    public List<User> sortByAllFields (List<User> users) {
+        class UserCompareByAllFields implements Comparator<User> {
+            public int compare(User lhs, User rhs) {
+                int result = lhs.getName().compareTo(rhs.getName());
+                if (result == 0) {
+                    result = lhs.getAge() > rhs.getAge() ? 1 : -1;
+                }
+                return result;
+            }
+        }
+        UserCompareByAllFields userCompareByAllFields = new UserCompareByAllFields();
+        Collections.sort(users, userCompareByAllFields);
+        return users;
+    }
 }
--- chapter_003/src/test/java/ru/job4j/sort/SortUserTest.java
+++ chapter_003/src/test/java/ru/job4j/sort/SortUserTest.java
@@ -32,12 +32,33 @@ public class SortUserTest {
         SortUser su = new SortUser();
         List<User> usersList = new ArrayList<>();
         usersList.add(new User("Vasya", 40));
-        usersList.add(new User("Petya", 20));
-        usersList.add(new User("Vova", 30));
-        usersList.add(new User("Vasya", 40));
+        usersList.add(new User("Petya", 30));
+        usersList.add(new User("Vova", 20));
+        usersList.add(new User("Vasya", 50));
         Set<User> tree = su.sort(usersList);
         User[] arrFromTree = new User[tree.size()];
         tree.toArray(arrFromTree);
-        assertThat(arrFromTree[0].getName(), is("Petya"));
+        assertThat(arrFromTree[0].getName(), is("Vova"));
+    }
+    @Test
+    public void whenSortByNameLength() {
+        SortUser su = new SortUser();
+        List<User> usersList = new ArrayList<>();
+        usersList.add(new User("Vasya", 40));
+        usersList.add(new User("V", 50));
+        usersList.add(new User("Vas", 20));
+        List<User> list = su.sortNameLength(usersList);
+        assertThat(list.get(0).getName(), is("V"));
+    }
+    @Test
+    public void whenSortByAllFields() {
+        SortUser su = new SortUser();
+        List<User> usersList = new ArrayList<>();
+        usersList.add(new User("Vova", 5));
+        usersList.add(new User("Vova", 30));
+        usersList.add(new User("Anya", 10));
+        usersList.add(new User("Anya", 20));
+        List<User> list = su.sortByAllFields(usersList);
+        assertThat(list.get(0).getAge(), is(10));
     }
 }
