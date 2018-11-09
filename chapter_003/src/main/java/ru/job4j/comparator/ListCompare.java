752fa286946c8e2424fe636850b5c18d370e7e6f
mrpuer-mini <vova@mytnik.net>
2018-11-09 21:38:12 +0300

    Chapter3/Collections: ListCompare

---
+++ chapter_003/src/main/java/ru/job4j/comparator/ListCompare.java
@@ -0,0 +1,26 @@
+package ru.job4j.comparator;
+
+import java.util.Comparator;
+
+/**
+ * Class description.
+ *
+ * @author Vladimir Mytnik (vova@mytnik.net).
+ * @version 0.1.
+ * @since 11/9/18
+ */
+public class ListCompare implements Comparator<String> {
+    @Override
+    public int compare(String left, String right) {
+        int result = 0;
+        int size = left.length() > right.length() ? right.length() : left.length();
+        for (int i = 0; i < size; i++) {
+            result = Character.compare(left.charAt(i), right.charAt(i));
+            if (result != 0 ) { break; }
+        }
+        if (result == 0 && left.length() != right.length()) {
+            result = left.length() > right.length() ? 1 : -1;
+        }
+        return result;
+    }
+}
